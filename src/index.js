0ffca86d83a0fec2e0cab7da173cb92ce72327e7
stehrenberg <sehrenbe@hm.edu>
2017-06-13 16:13:51 +0200

    Make square display 'X' upon click

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import './index.css';
 
 class Square extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            value: null
+        };
+    }
+
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
@@ -17,7 +24,7 @@ class Square extends React.Component {
 class Board extends React.Component {
 
     renderSquare(i) {
-        return <Square/>;
+        return <Square value={i} />;
     }
 
     render() {
